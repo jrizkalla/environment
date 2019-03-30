b3585cf3a2bc10dd88809317b2ae251aea41182c
John Rizkalla <johnrizkalla@Johns-Macbook-Pro.local>
2019-03-30 14:35:06 -0700

    Added init_vim_on_ssh.sh

---
+++ bash/init_vim_on_ssh.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# Script to install a basic Vimrc on a new system (over ssh)
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 username@hostname[:port]" >&2
+    exit 1
+fi
+
+hostname="$1"
+vimrc="$(mktemp)"
+
+function _cleanup {
+    rm -f "$vimrc"
+}
+
+trap _cleanup EXIT
+
+cat > "$vimrc" <<EOF
+
+set nocompatible
+syntax on
+set number
+set relativenumber
+
+inoremap jk <esc>
+inoremap jK <esc>
+inoremap Jk <esc>
+inoremap JK <esc>
+
+set autoindent
+set tabstop=4
+set shiftwidth=4
+set expandtab
+
+set smartcase
+
+EOF
+
+scp "$vimrc" "$hostname":~/.vimrc
